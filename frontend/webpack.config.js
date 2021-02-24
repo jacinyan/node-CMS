718549588eee9b2abf00336a92432d773367c385
Jacin <jacinjiyan@gmail.com>
2021-02-24 21:30:06 +1100

    dist path issues persists

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,17 +1,38 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
 
 module.exports = {
     mode: 'development',
     entry: {
         app: './src/app.js'
-    }, 
+    },
     output: {
         path: path.join(__dirname, './dist'),
         filename: 'app.js'
     },
     // plug-ins & loaders
     plugins: [
-        new HtmlWebpackPlugin()
-    ]
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, './public/index.html'),
+            filename: 'app.html',
+            inject: true
+        }),
+        new CopyPlugin(
+            {
+                patterns:
+                    [{
+                        from: path.join(__dirname, './public/*.ico'),
+                        to: path.join(__dirname, './dist/'),
+                    }]
+            }
+        )
+    ],
+    // server
+    devServer: {
+        contentBase: path.join(__dirname, './dist'),
+        compress: true,
+        port: 9000
+    }
 }
